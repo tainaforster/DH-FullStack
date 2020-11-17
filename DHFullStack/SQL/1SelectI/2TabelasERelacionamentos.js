da667d621684c5ca1b9521a41c909a425ace42f7
tainaforster <tainaforster@gmail.com>
2020-11-16 22:12:06 -0300

    Adicionando SQL-Select I

--- DHFullStack/SQL/1SelectI/2TabelasERelacionamentos.js
+++ DHFullStack/SQL/1SelectI/2TabelasERelacionamentos.js
@@ -1,3 +1,41 @@
 // Como modelar (entender a estrutura com a qual essas informaçoes serao armazenadas) um banco de dados:
 
-// Um dos metodos mais comuns é o DIAGRAMA RELACIONAL
+// Um dos metodos mais comuns é o DIAGRAMA RELACIONAL, esse diagrama é uma abstraçao do que é 
+// conhecido como tabela fisica, que é a tabela real do banco de dados.
+
+//  Esses diagramas permitem entender de forma rapida e simples todas as tabelas de nosso banco
+//  de dados.
+
+
+// Estrutura:
+
+// *** Entidades - Dentro mdo sistema teremos entidades, que serao a representação de um objeto ou coisa
+//  da vida real. Para integra-los em nosso diagrama usa-se retangulos.
+// Para o nome destas tabelas usa-se substantivos no plural.
+
+// *** Atributos - Estas sao as caracteristicas que irao definir cada entidade. Por exemplo: em uma
+//  entidade FILMES poderiam ter os seguintes atributos: titulo, avaliaçao, data-de-lançamento,
+//  pais-de-origem...
+
+// *** Chave primaria (PK - Primary key) - É um campo (ou combinaçao de campos) que identifica de 
+// forma exclusiva cada linha em uma tabela. Idto significa que nao pode haver duas linha em uma 
+// tabela com o mesmo PK. 
+//  Para identificar a chave primaria em uma entidade, podemos escrever um atributo em NEGRITO
+// seguido da iniciais (PK), em negrito tambem.
+
+
+//  Relaçoes no diagrama - elas indicam como se relacinar com duas tabelas.
+
+
+//  Em banco de dados temos 3 tipos de relaçoes:
+
+// - Um para um;
+// - Um para muitos;
+// - Muitos para muitos.
+
+
+// Sempre que fazemos uma relaçao entre as tabelas devemos usar as chaves primarias.
+
+// Chave estrangeira ou chave externa (FK) - se refere ao tipo de relacionamento entre distintas 
+// tabelas de dados do banco de dados. Uma chave estrangeira é chamada quando há o relacionamento 
+// entre duas tabelas.
