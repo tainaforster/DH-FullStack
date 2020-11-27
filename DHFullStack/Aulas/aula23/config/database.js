d7e1bda0664495079f5d0027391dd24c6eb1dff6
tainaforster <tainaforster@gmail.com>
2020-11-26 22:03:53 -0300

    Adicionando aula 22 e 23

---
+++ DHFullStack/Aulas/aula23/config/database.js
@@ -0,0 +1,10 @@
+const config = {
+    username: 'root',
+    password: '890509',
+    database: 'dh_node_fs10',
+    host: 'localhost',
+    dialect: 'mysql' 
+  }
+  
+  module.exports = config;
+  
